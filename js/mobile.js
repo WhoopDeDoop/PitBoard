008124eb0fe01ac81101d300ab31d2b6dab0b11c
Ashton Poindexter <ashton.poindexter@gmail.com>
2023-02-04 22:28:43 -0500

    Updated

---
+++ js/mobile.js
@@ -0,0 +1,16 @@
+//  <--- Mobile Version of PitBoard resource --->  // 
+
+// This is an additonal resource for the mobile //
+// version of PitBoard.                         //
+
+// Dev Rules
+let streamHidden = false;
+
+$(document).ready(function() {
+    // On DOM ready
+    
+});
+
+$(window).on("load", function(){
+    // Page Fully Loaded
+});
